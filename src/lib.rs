c7948577cb412776884db9bbbacbcb19a14021c1
Al <gLiu962@aucklanduni.ac.nz>
2022-01-05 00:12:02 +0800

    upload template

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+//! A template for creating Rust open-source repo on GitHub
+//!
+#![doc(html_root_url = "https://docs.rs/template-rs/0.1.0")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
+#![deny(missing_docs)]
+
+/// template
+pub fn it_works() -> usize {
+    4
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_works() {
+        assert_eq!(it_works(), 4);
+    }
+}

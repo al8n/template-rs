ced3ca3e3631fb6a1d51d4ff06e73ae3595fbc1a
Al Liu <scygliu1@gmail.com>
2023-10-06 02:27:44 +0800

    update template

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 //! A template for creating Rust open-source repo on GitHub
-//!
-#![doc(html_root_url = "https://docs.rs/template-rs/0.1.2")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![deny(missing_docs)]

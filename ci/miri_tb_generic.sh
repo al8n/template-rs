e1deb937101eb14b2fcc66974a6228c86028059b
Al Liu <scygliu1@gmail.com>
2024-09-15 19:23:59 +0800

    Update CI

--- ci/miri_tb_generic.sh
+++ ci/miri_tb_generic.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]; then
+  echo "Error: CONFIG_FLAGS are not provided"
+  exit 1
+fi
+
+CONFIG_FLAGS=$1
+
 rustup toolchain install nightly --component miri
 rustup override set nightly
 cargo miri setup
 
 export MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-disable-isolation -Zmiri-symbolic-alignment-check -Zmiri-tree-borrows"
+export RUSTFLAGS="--cfg test_$CONFIG_FLAGS"
 
-cargo miri test --tests --all-features
+cargo miri test --tests --lib
--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -5,13 +5,17 @@ set -ex
 export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
 # Run address sanitizer
-RUSTFLAGS="-Z sanitizer=address" \
+RUSTFLAGS="-Z sanitizer=address --cfg all_tests" \
 cargo test --tests --target x86_64-unknown-linux-gnu --all-features
 
 # Run leak sanitizer
-RUSTFLAGS="-Z sanitizer=leak" \
+RUSTFLAGS="-Z sanitizer=leak --cfg all_tests" \
+cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+
+# Run memory sanitizer
+RUSTFLAGS="-Z sanitizer=memory --cfg all_tests" \
 cargo test --tests --target x86_64-unknown-linux-gnu --all-features
 
 # Run thread sanitizer
-RUSTFLAGS="-Z sanitizer=thread" \
+RUSTFLAGS="-Z sanitizer=thread --cfg all_tests" \
 cargo -Zbuild-std test --tests --target x86_64-unknown-linux-gnu --all-features
